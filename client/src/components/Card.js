249bee26e6746ae6eae81a65732ed2db7505f4de
gnomemage1990@gmail.com <gnomemage1990@gmail.com>
2024-04-16 19:39:13 +0800

    commit

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -11,18 +11,18 @@ function Card({
     <>
         <div className="flex hover:border-2 rounded cursor-pointer px-6 py-2" >
             <div className="min-h-[100px] min-w-[100px]">
-            <img src={image_path} />
+                <img src={image_path} />
             </div>
             <div className="flex flex-col">
-            <div className="">
-                Name: {name}
-            </div>
-            <div className="">
-                Description: {description}
-            </div>
-            <div className="">
-                Contained in: {container}
-            </div>
+                <div className="">
+                    Name: {name}
+                </div>
+                <div className="">
+                    Description: {description}
+                </div>
+                <div className="">
+                    Contained in: {container}
+                </div>
             </div>
         </div>
     </>
--- client/src/pages/item.js
+++ client/src/pages/item.js
@@ -1,8 +1,9 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useContext } from 'react'
 import { base } from '../utils/constant'
 import Update from '../components/Update';
 import Card from '../components/Card'
 import { BrowserRouter as Router, Route, Link, Switch, useParams } from 'react-router-dom';
+import { GlobalContext } from '../App'
 
 function Item() {
 
@@ -10,7 +11,7 @@ function Item() {
     view: "View",
     update: "Update"
   }
-
+  const global_context = useContext(GlobalContext)
   const { id } = useParams();
   console.log("id", id)
   const [data, setData]=useState([])
@@ -25,6 +26,31 @@ function Item() {
     })
   },[])
 
+  const deleteItem=()=>{
+    try {
+      fetch(`${base}/api/item/${id}`, { 
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response)=>response.json())
+      .then((response)=>{
+        console.log("response", response)
+        if (response?.success) {
+          global_context.toast("Successfully Deleted")
+          setTimeout(()=>{
+              document.location.href = `/items`
+          }, 5000)
+        } else {
+            global_context.toast("Error")
+        }
+      })
+    } catch(err) {
+      console.log("err", err)
+      global_context.toast("Error")
+    }
+  }
+
   return (
     <div className="h-screen px-12 py-8">
       <h2><u></u></h2>
@@ -33,6 +59,7 @@ function Item() {
             {...data}
           />
           <button className="submit-btn" onClick={()=>setMode(_mode.update)}>Update</button>
+          <button className="submit-btn" onClick={deleteItem}>Delete</button>
         </>}
         {mode == _mode.update && <Update id={id} name={data?.name} type="item" />}
         {/* <button className="submit-btn" onClick={
